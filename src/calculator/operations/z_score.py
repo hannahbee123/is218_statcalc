a083e042a29f5dfa37c9bcba60655a61c7872cbc
JDavid1125 <76935030+JDavid1125@users.noreply.github.com>
2021-03-07 07:09:12 -0500

    Feature: Added added Z-score code

--- src/calculator/operations/z_score.py
+++ src/calculator/operations/z_score.py
@@ -1,1 +1,4 @@
-# def find_z(values):
+def find_z(values, a):
+    values_mean = sum(values) / len(values)
+    standard_dev = (sum([((x - values_mean) ** 2) for x in values]) / len(values)) ** 0.5
+    return (a-values_mean) / standard_dev
