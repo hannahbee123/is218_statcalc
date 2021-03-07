c0e9db340c007b2438568e96bb7d4c4422030c72
JDavid1125 <76935030+JDavid1125@users.noreply.github.com>
2021-03-07 06:52:06 -0500

    Feature: Added median code

--- src/calculator/operations/median_calc.py
+++ src/calculator/operations/median_calc.py
@@ -1,1 +1,7 @@
-# def find_median(values):
+def find_median(values):
+    sorted_values = values.sort()  # sorts values numerically
+    length = len(values)
+    if (length % 2) == 0:  # checks if amount of values is even
+        return ((sorted_values[length/2])+(sorted_values[(length/2)-1])) / 2
+    else:
+        return sorted_values[(length/2) - 1.5]
