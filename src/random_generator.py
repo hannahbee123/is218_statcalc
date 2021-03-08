3801342c463cf3688f04537e8bed7e19c6e1c864
hbenjamin529 <31874276+hbenjamin529@users.noreply.github.com>
2021-03-08 15:32:55 -0500

    Task: reorganized comments in random generator file

--- src/random_generator.py
+++ src/random_generator.py
@@ -1,27 +1,16 @@
-# random generator class
-'''
-functions:
-1. Generate a random number without a seed between a range of two numbers -- both integer and decimal
-2. Generate a random number with a seed between a range of two numbers -- both integer and decimal
-3. Generate a list of N random numbers with a seed and between a range of numbers -- both integer and decimal
-4. Select a random number from a list
-5. Set a seed and randomly select the same value from a list
-6. Select N number of items from a list without a seed
-7. Select N number of items from a list with a seed
-'''
 import random
 
 
-def rand(a, b):
+def rand(a, b):   # generate random number without a seed between range of two numbers
     return random.randrange(a, b)
 
 
-def rand_seed(a, b, seed):
+def rand_seed(a, b, seed):   # generate random number with seed between range of two numbers
     random.seed(seed)
     return random.randrange(a, b)
 
 
-def rand_list(a, b, n, seed):
+def rand_list(a, b, n, seed):   # generate list of n random numbers with seed and between range of numbers
     random.seed(seed)
     random_list = []
     for value in range(n):
@@ -29,19 +18,19 @@ def rand_list(a, b, n, seed):
     return random_list
 
 
-def rand_choice(lst):
+def rand_choice(lst):   # select random number from list
     return random.choice(lst)
 
 
-def rand_choice_seed(lst, seed):
+def rand_choice_seed(lst, seed):   # set a seed and randomly select the same value from a list
     random.seed(seed)
     return random.choice(lst)
 
 
-def rand_choices(lst, n):
+def rand_choices(lst, n):   # select N number of items from a list without a seed
     return random.choices(lst, k=n)
 
 
-def rand_choices_seed(lst, n, seed):
+def rand_choices_seed(lst, n, seed):   # select N number of items from a list with a seed
     random.seed(seed)
     return random.choices(lst, k=n)
