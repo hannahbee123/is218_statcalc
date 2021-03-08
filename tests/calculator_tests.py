5b21b786069ce5ac53ed07556b186c279b87804a
hbenjamin529 <31874276+hbenjamin529@users.noreply.github.com>
2021-03-08 16:19:19 -0500

    Feature: unit tests for mean, std dev, and variance

--- tests/calculator_tests.py
+++ tests/calculator_tests.py
@@ -44,15 +44,27 @@ class CalculatorTestCase(unittest.TestCase):
         self.assertEqual(calculator.square_root(4), 2)
         self.assertEqual(calculator.result, 2)
 
-    # def test_mean_method_calculator(self):
+    def test_mean_method_calculator(self):
+        calculator = Calculator()
+        test_list = [4, 4, 1, 2, 6, 12, 13]
+        self.assertEqual(calculator.mean(test_list), 6)
+        self.assertEqual(calculator.result, 6)
 
     # def test_median_method_calculator(self):
 
     # def test_mode_method_calculator(self):
 
-    # def test_std_dev_method_calculator(self):
+    def test_std_dev_method_calculator(self):
+        calculator = Calculator()
+        test_list = [4, 4, 1, 2, 6, 12, 13]
+        self.assertEqual(calculator.standard_dev(test_list), 4.375255094603872)
+        self.assertEqual(calculator.result, 4.375255094603872)
 
-    # def test_variance_method_calculator(self):
+    def test_variance_method_calculator(self):
+        calculator = Calculator()
+        test_list = [4, 4, 1, 2, 6, 12, 13]
+        self.assertEqual(calculator.variance(test_list), 19.142857142857142)
+        self.assertEqual(calculator.result, 19.142857142857142)
 
     # def test_z_score_method_calculator(self):
 
