cecb019031f01243ba7f2e51b471c26fd9499cd8
JDavid1125 <76935030+JDavid1125@users.noreply.github.com>
2021-03-19 10:09:02 -0400

    Feature: Added Cochran's Formula and finding sample size given confidence interval and width

--- src/population_sampling.py
+++ src/population_sampling.py
@@ -33,6 +33,36 @@ class PopulationSampling:
         margin_err = 1.96 * (std_dev / calculator.square_root(n))
         print('The confidence interval is' + mean + '+/-' + margin_err)
 
-    # TO DO (juan):
-    # cochran sample size formula
-    # find sample size given confidence interval and width (unknown population std dev)
+    @staticmethod
+    def cochran_formula(self, lst, p):
+        """
+        This method returns the ideal sample size of a list using a confidence z-score of 1.96.
+        Probability must be greater than 0 and less than 1. If not then the
+        probability will be assumed to be 50 percent (i.e. p = 0.5)
+        if the population is less than a 10,000 then the modified Cochran's formula for smaller populations
+        will be instead
+        """
+        if p >= 1 or p <= 0:
+            p = 0.5
+        calculator = Calculator()
+        std_dev = calculator.standard_dev(lst)
+        n = len(lst)
+        z = 1.96
+        margin_err = z * (std_dev / calculator.square_root(n))
+        n0 = round(((z ** 2)*p*(1-p))/(margin_err ** 2))
+        if n < 10000:  # checks if the population is small
+            return round(n0/(1+(n0-1/n)))
+        else:
+            return n0
+
+    @staticmethod
+    def confidence_width(self, width, p):
+        """
+        Finds sample size given confidence interval and width (unknown population std dev).
+        Due to the lack of a standard dev, the confidence interval will be 95%
+        """
+        if p >= 1 or p <= 0:
+            p = 0.5
+        e = width/2
+        z = 1.96
+        return round(((z/e) ** 2) * (p * (1-p)))
